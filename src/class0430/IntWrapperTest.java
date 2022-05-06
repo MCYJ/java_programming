f8db7e22de81c7a2c0f5b0b0a14332d9abefa9d6
MCYJ <june1012june@gmail.com>
2022-05-07 05:31:58 +0900

    feat : code for wrapper

---
+++ src/class0430/IntWrapperTest.java
@@ -0,0 +1,8 @@
+package class0430;
+
+public class IntWrapperTest {
+	public static void main(String[] args) {
+		int i = Integer.parseInt("10");
+		System.out.println(i);
+	}
+}
---
+++ src/class0430/Main.java
@@ -0,0 +1,12 @@
+package class0430;
+
+public class Main {
+	public static void main(String[] args) {
+		Point po = new Point();
+		System.out.println(po.getClass().getName());
+	}
+}
+
+class Point{
+	
+}
