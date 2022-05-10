772b03c657a6d71f310899e932d57b02d57c1ed9
MCYJ <june1012june@gmail.com>
2022-05-10 22:10:20 +0900

    feat : codes for java

---
+++ src/class0509/ArrayListEx.java
@@ -0,0 +1,32 @@
+package class0509;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class ArrayListEx {
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<String>();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for (int i = 0; i < 4; i++) {
+			System.out.print("이름을 입력하세요>>");
+			String s = scanner.next();
+			a.add(s);
+		}
+		
+		for (int i = 0; i < a.size(); i++) {
+			String name = a.get(i);
+			System.out.println(name+" ");
+		}
+		
+		int longestIndex = 0;
+		for (int i = 0; i < a.size(); i++) {
+			if(a.get(longestIndex).length() < a.get(i).length())
+				longestIndex = i;
+		}
+		
+		System.out.println("\n가장 긴 이름은:"+a.get(longestIndex));
+		scanner.close();
+	}
+}
